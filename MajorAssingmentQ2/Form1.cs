bcfc6737ce75b0c32bd7d46645e76786a803f27a
Umair Jibran <umairjibran7@gmail.com>
2020-05-12 09:27:57 +0500

    Added Dummy Programs

---
+++ MajorAssingmentQ2/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MajorAssingmentQ2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
