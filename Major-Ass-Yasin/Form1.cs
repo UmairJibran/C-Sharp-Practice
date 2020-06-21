3346a5568902adf02a8c0c60cda1feda8227dea0
Umair Jibran <umairjibran7@gmail.com>
2020-06-21 23:00:30 +0500

    Major Assignment Yasin

---
+++ Major-Ass-Yasin/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Major_Ass_Yasin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var today = dateTimePicker2.Value.Year;
+            var byear = dateTimePicker1.Value.Year;
+            var age = today - byear;
+            textBox1.Text = "" + age;
+        }
+    }
+}
