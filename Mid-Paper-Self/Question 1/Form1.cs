f8434b09e614353bc483f31681b8e1dceb9fc1c1
Umair Jibran <umairjibran7@gmail.com>
2020-06-24 14:33:09 +0500

    Finalised Question 1

--- Mid-Paper-Self/Question 1/Form1.cs
+++ Mid-Paper-Self/Question 1/Form1.cs
@@ -11,51 +11,39 @@ using System.Windows.Forms;
 
 namespace Question_1
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
+    public partial class Form1 : Form{
+        public Form1(){
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (File.Exists(@"../.././8440.txt"))
-                MessageBox.Show("File Exists");
-            else
-                File.Create(@"../.././8440.txt");
+        //8440 Umair Jibran
+        private void button1_Click(object sender, EventArgs e){
+            if (File.Exists(@"../.././8440.txt")) MessageBox.Show("File Exists");
+            else File.Create(@"../.././8440.txt");
         }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (File.Exists(@"../.././8440.txt"))
-            {
+        private void button2_Click(object sender, EventArgs e){
+            if (File.Exists(@"../.././8440.txt")){
                 File.Copy(@"../.././8440.txt", @".././8440.txt");
                 MessageBox.Show("Copied Succesfully");
             }
-            else
-                MessageBox.Show("No Such File Exists");
+            else MessageBox.Show("No Such File Exists");
 
         }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            if (File.Exists(@"../.././8440.txt"))
-            {
+        private void button3_Click(object sender, EventArgs e){
+            if (File.Exists(@"../.././8440.txt")){
                 File.Move(@"../.././8440.txt", @".././8440.txt");
                 MessageBox.Show("Moved Succesfully");
             }
-            else
-                MessageBox.Show("No Such File Exists");
+            else MessageBox.Show("No Such File Exists");
         }
-
-        private void button4_Click(object sender, EventArgs e)
-        {
-            String message = textBox1.Text;
-            TextWriter txt = new StreamWriter(@"../.././8440.txt");
-            txt.Write(message);
-            txt.Close();
-            MessageBox.Show(message + " Written Succesfully");
+        private void button4_Click(object sender, EventArgs e){
+            if (File.Exists(@"../.././8440.txt")){
+                String message = textBox1.Text;
+                TextWriter txt = new StreamWriter(@"../.././8440.txt");
+                txt.Write(message);
+                txt.Close();
+                MessageBox.Show(message + " Written Succesfully");
+            }
+            else MessageBox.Show("File Doesn't Exist Please Create it First");
         }
     }
 }
