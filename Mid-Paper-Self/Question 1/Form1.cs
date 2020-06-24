e6065828e3d46263ca76e3965f8a0612a64b5083
Umair Jibran <umairjibran7@gmail.com>
2020-06-24 14:21:36 +0500

    completed question 1

--- Mid-Paper-Self/Question 1/Form1.cs
+++ Mid-Paper-Self/Question 1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,46 @@ namespace Question_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"../.././8440.txt"))
+                MessageBox.Show("File Exists");
+            else
+                File.Create(@"../.././8440.txt");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"../.././8440.txt"))
+            {
+                File.Copy(@"../.././8440.txt", @".././8440.txt");
+                MessageBox.Show("Copied Succesfully");
+            }
+            else
+                MessageBox.Show("No Such File Exists");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(@"../.././8440.txt"))
+            {
+                File.Move(@"../.././8440.txt", @".././8440.txt");
+                MessageBox.Show("Moved Succesfully");
+            }
+            else
+                MessageBox.Show("No Such File Exists");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String message = textBox1.Text;
+            TextWriter txt = new StreamWriter(@"../.././8440.txt");
+            txt.Write(message);
+            txt.Close();
+            MessageBox.Show(message + " Written Succesfully");
         }
     }
 }
