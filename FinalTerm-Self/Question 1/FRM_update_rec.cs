eec2f642b43ad6db3f2a492497b51c1e632743e4
Umair Jibran <umairjibran7@gmail.com>
2020-06-26 17:02:57 +0500

    removed white spaces

--- FinalTerm-Self/Question 1/FRM_update_rec.cs
+++ FinalTerm-Self/Question 1/FRM_update_rec.cs
@@ -18,21 +18,16 @@ namespace Question_1
             InitializeComponent();
         }
         MySqlConnection conn = new MySqlConnection("datasource=localhost;port=3306;username=root;password=root");
-        private void FRM_update_rec_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn_update_Click(object sender, EventArgs e)
-        {
+        private void FRM_update_rec_Load(object sender, EventArgs e){txtbx_id.Focus();}
+        private void btn_update_Click(object sender, EventArgs e){
             conn.Open();
-            if(conn.State == ConnectionState.Open)
-            {
+            if(conn.State == ConnectionState.Open){
                 int id = int.Parse(txtbx_id.Text);
                 string first_name = txtbx_fname.Text;
                 string last_name = txtbx_lname.Text;
                 int age = int.Parse(txtbx_age.Text);
-                string sql = "UPDATE `final_exam`.`student_table` SET `student_first_name` = '"+first_name+"', `student_last_name` = '"+last_name+"', `student_age`="+age+" WHERE (`student_id` = '" + id+"');";
+                string sql = "UPDATE `final_exam`.`student_table` SET `student_first_name` = '"+first_name+"', `student_last_name` = '"
+                    +last_name+"', `student_age`="+age+" WHERE (`student_id` = '" + id+"');";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 cmd.ExecuteNonQuery();
                 txtbx_age.Clear();
@@ -40,11 +35,7 @@ namespace Question_1
                 txtbx_id.Clear();
                 txtbx_lname.Clear();
                 conn.Close();
-            }
-            else
-            {
-                MessageBox.Show("Connection Failed");
-            }
+            }else MessageBox.Show("Connection Failed");
         }
     }
 }
