859f87ecb9f81dd88679212b245a1c1cea287b48
Umair Jibran <umairjibran7@gmail.com>
2020-06-26 15:30:02 +0500

    connected Database in insertion form

--- FinalTerm-Self/Question 1/frm_insert.cs
+++ FinalTerm-Self/Question 1/frm_insert.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Question_1
 {
@@ -16,10 +17,18 @@ namespace Question_1
         {
             InitializeComponent();
         }
-
+        MySqlConnection conn = new MySqlConnection("datasource=localhost;port=3306;username=root;password=root");
         private void frm_insert_Load(object sender, EventArgs e)
         {
-            txtbx_id.Focus();
+            conn.Open();
+            if (conn.State==ConnectionState.Open)
+            {
+                txtbx_id.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Database couldn't be opened!");
+            }
         }
 
         private void btn_insert_record_Click(object sender, EventArgs e)
