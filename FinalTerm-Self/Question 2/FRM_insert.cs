7505a72b9d3660a69d46fd3f211d7aa0bcc8f94a
Umair Jibran <umairjibran7@gmail.com>
2020-06-26 18:33:20 +0500

    Update FRM_insert.cs

--- FinalTerm-Self/Question 2/FRM_insert.cs
+++ FinalTerm-Self/Question 2/FRM_insert.cs
@@ -18,27 +18,22 @@ namespace Question_2
             InitializeComponent();
         }
         MySqlConnection conn = new MySqlConnection("datasource=localhost;port=3306;username=root;password=root");
-        private void btn_add_Click(object sender, EventArgs e)
-        {
+        private void btn_add_Click(object sender, EventArgs e){
             string name = txtbx_name.Text;
             string rprice = txtbx_rprice.Text;
             string pprice = txtbx_pprice.Text;
             string qty = txtbx_qty.Text;
-            if (conn.State == ConnectionState.Open)
-            {
-                string sql = "INSERT INTO `final_exam`.`stationary_store` (`item_name`, `item_purchase_price`, `item_retail_price`, `item_quantity`) VALUES ('"+name+"', '"+rprice+"', '"+pprice+"', '"+qty+"');";
+            if (conn.State == ConnectionState.Open){
+                string sql = "INSERT INTO `final_exam`.`stationary_store` (`item_name`, `item_purchase_price`, `item_retail_price`," +
+                    "`item_quantity`) VALUES ('"+name+"', '"+rprice+"', '"+pprice+"', '"+qty+"');";
                 MySqlCommand cmd = new MySqlCommand(sql, conn);
                 cmd.ExecuteNonQuery();
             }
         }
 
-        private void FRM_insert_Load(object sender, EventArgs e)
-        {
-            conn.Open();
-        }
+        private void FRM_insert_Load(object sender, EventArgs e){conn.Open();}
 
-        private void btn_rst_Click(object sender, EventArgs e)
-        {
+        private void btn_rst_Click(object sender, EventArgs e){
             txtbx_name.Clear();
             txtbx_pprice.Clear();
             txtbx_rprice.Clear();
