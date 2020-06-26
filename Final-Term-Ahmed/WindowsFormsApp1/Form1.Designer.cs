bb85de9b62805f0340998cc6492187bb3394bff2
Umair Jibran <umairjibran7@gmail.com>
2020-06-26 23:25:44 +0500

    completed q1

--- Final-Term-Ahmed/WindowsFormsApp1/Form1.Designer.cs
+++ Final-Term-Ahmed/WindowsFormsApp1/Form1.Designer.cs
@@ -130,6 +130,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "View";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -139,6 +140,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Search";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -148,6 +150,7 @@
             this.button4.TabIndex = 2;
             this.button4.Text = "Delete";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -157,6 +160,7 @@
             this.button5.TabIndex = 2;
             this.button5.Text = "Update";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
--- Final-Term-Ahmed/WindowsFormsApp1/Form1.cs
+++ Final-Term-Ahmed/WindowsFormsApp1/Form1.cs
@@ -34,5 +34,49 @@ namespace WindowsFormsApp1
             var command = new MySqlCommand(query, connection);
             command.ExecuteNonQuery();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=root");
+            MySqlDataAdapter myadapter = new MySqlDataAdapter("SELECT * FROM myfinalpaper.question1", connection);
+            connection.Open();
+            DataSet ds = new DataSet();
+            myadapter.Fill(ds, "question1");
+            dataGridView1.DataSource = ds.Tables["question1"];
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string id = textBox1.Text;
+            MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=root");
+            connection.Open();
+            string query = "DELETE FROM myfinalpaper.question1 WHERE `id` = '" + id + "';";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.ExecuteNonQuery();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=root");
+            connection.Open();
+            string id = textBox1.Text;
+            string fname = textBox2.Text;
+            string lname = textBox3.Text;
+            string age = textBox4.Text;
+            string query = "UPDATE `myfinalpaper`.`question1` SET `fname` = '" + fname + "', `lname` = '" + lname + "', `age` = '" + age + "' WHERE (`id` = '" + id+"');";
+            MySqlCommand command = new MySqlCommand(query, connection);
+            command.ExecuteNonQuery();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string name = textBox5.Text;
+            MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=root");
+            MySqlDataAdapter myadapter = new MySqlDataAdapter("SELECT * FROM myfinalpaper.question1 WHERE `fname` = '"+name+"';", connection);
+            connection.Open();
+            DataSet ds = new DataSet();
+            myadapter.Fill(ds, "question1");
+            dataGridView1.DataSource = ds.Tables["question1"];
+        }
     }
 }
