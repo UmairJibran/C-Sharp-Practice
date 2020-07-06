65f382eb5b6ab899f38511d2341efb3c7e5bad62
Umair Jibran <umairjibran7@gmail.com>
2020-07-06 14:01:35 +0500

    theory final exam, kamal

---
+++ FinalTerm-Kamal/Kamaluddin_shinwari/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Kamaluddin_shinwari
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.kamaluddin_shinwariDataSet = new Kamaluddin_shinwari.kamaluddin_shinwariDataSet();
+            this.kamaluddinshinwariDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.kamaluddin_shinwariDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kamaluddinshinwariDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kamaluddin_shinwariDataSet
+            // 
+            this.kamaluddin_shinwariDataSet.DataSetName = "kamaluddin_shinwariDataSet";
+            this.kamaluddin_shinwariDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // kamaluddinshinwariDataSetBindingSource
+            // 
+            this.kamaluddinshinwariDataSetBindingSource.DataSource = this.kamaluddin_shinwariDataSet;
+            this.kamaluddinshinwariDataSetBindingSource.Position = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(367, 301);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 325);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "Form1";
+            this.Text = "Yasin - 8545";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kamaluddin_shinwariDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kamaluddinshinwariDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource kamaluddinshinwariDataSetBindingSource;
+        private kamaluddin_shinwariDataSet kamaluddin_shinwariDataSet;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ FinalTerm-Kamal/Kamaluddin_shinwari/Form1.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kamaluddin_shinwari
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        MySqlConnection connection = new MySqlConnection("datasource=localhost;port=3306;username=root;password=root");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        connection.Open();
+            MySqlDataAdapter myadapter = new MySqlDataAdapter("SELECT * FROM  yasin.yasin_table", connection);
+            DataSet ds = new DataSet();
+            myadapter.Fill(ds, "yasin_table");
+            dataGridView2.DataSource = ds.Tables["yasin_table"];
+        }
+    }
+}
