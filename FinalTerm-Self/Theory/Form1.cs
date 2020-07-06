5eb98943afd6440d21a887ef54196c3c5cbf3733
Umair Jibran <umairjibran7@gmail.com>
2020-07-06 14:26:45 +0500

    final term theory, self

---
+++ FinalTerm-Self/Theory/Form1.cs
@@ -0,0 +1,36 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Theory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MySqlConnection conn = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=root");
+        private void Form1_Load(object sender, EventArgs e){
+            try{
+                MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM db_umair.tbl_umair", conn);
+                conn.Open();
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "tbl_umair");
+                dataGridView1.DataSource = ds.Tables["tbl_umair"];
+                conn.Close();
+            }
+            catch (Exception ex){
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
